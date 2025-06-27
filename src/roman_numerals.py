a068d8436a372aabb0c44fbc649f73ba179a5f3e
Sergey Kudashev <kudashevs@gmail.com>
2025-06-27 23:34:04 +0200

    refactor: extract a _convert_to_roman method

--- src/roman_numerals.py
+++ src/roman_numerals.py
@@ -6,4 +6,11 @@ class RomanNumerals:
         if amount <= 0 or amount >= 4000:
             raise ValueError(f'{amount} cannot be converted a roman number')
 
-        return 'I'
+        return self._convert_to_roman(amount)
+
+
+    def _convert_to_roman(self, amount: int) -> str:
+        if amount <= 0:
+            return ''
+
+        return 'I' + self._convert_to_roman(amount - 1)
